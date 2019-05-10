648b0d2371ba7ee748949c47d475346d4db0534d
Blake Burroughs <b.burro@gmail.com>
2019-05-10 16:45:48 -0500

    201 created user but table not syncing?

---
+++ db/models/Course.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Course = sequelize.define('Course', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    estimatedTime: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    materialsNeeded: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+  });
+
+  Course.associate = (models) => {
+   // TODO Add associations.
+   Course.belongsTo(models.User, {
+    foreignKey: {
+      fieldName: 'id',
+      allowNull: false,
+    }});
+  };
+
+  return Course;
+};
