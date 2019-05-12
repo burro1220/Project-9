fef9fa627d3e1f146a95ed3683b2fdc9ae4b69ed
Blake Burroughs <b.burro@gmail.com>
2019-05-11 19:39:15 -0500

    Working on user route

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const User = require("../models").User;
 const { sequelize } = require("../models");
 
-router.get("/",(req, res) => {
+
+//GET current authorized User
+router.get("/", (req, res) => {
     res.json({
       id: req.currentUser.id,
       firstName: req.currentUser.emailAddress,
@@ -11,4 +13,33 @@ router.get("/",(req, res) => {
       emailAddress: req.currentUser.emailAddress
     });
     res.status(200);
-  });
+});
+
+// POST Create User
+router.post("/users", (req, res, next) => {
+    const { info } = req;
+    User.findOne({ where: { emailAddress: info.emailAddress }})
+        .then( email => {
+            if (email) {
+                res.json({ error: "This email address is already in use"});
+                res.status(400);
+            } else {
+               User.create({
+                   emailAddress: info.emailAddress,
+                   firstName: info.firstName,
+                   lastName: info.lastName,
+                   password: info.password
+               });
+               res.status(201).end();
+            }
+        })
+        .catch (err => {
+            if (err.name === "SequelizeValidationError") {
+                err.message = "All data must be entered";
+                err.status = 400;
+            } else {
+                next(err);
+            }
+        })
+
+})
