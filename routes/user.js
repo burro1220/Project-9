ae7ab37bfad8d7bd03a3b00ea8ac935e6ffc0f48
Blake Burroughs <b.burro@gmail.com>
2019-05-11 19:19:57 -0500

    working on user route

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models").User;
-const Sequelize = require("sequelize");
+const { sequelize } = require("../models");
 
-router.get('/', (req, res) => {
-   // console.log(res)
-    })
+router.get("/",(req, res) => {
+    res.json({
+      id: req.currentUser.id,
+      firstName: req.currentUser.emailAddress,
+      lastName: req.currentUser.lastName,
+      emailAddress: req.currentUser.emailAddress
+    });
+    res.status(200);
+  });
