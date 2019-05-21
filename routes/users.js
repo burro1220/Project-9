7c101405423e0d11c8f67665ed989410e267f187
Blake Burroughs <b.burro@gmail.com>
2019-05-21 14:53:24 -0500

    users routes broken

--- routes/users.js
+++ routes/users.js
@@ -14,10 +14,10 @@ router.get("/", authenticate, (req, res) => {
       emailAddress: req.currentUser.emailAddress
     });
     res.status(200);
-});
+    next(err);
 
 // POST Create User
-router.post("/", (req, res, next) => {
+router.post("/", authenticate, (req, res, next) => {
     const info = req.body;
         
     //If email address is undefined 
